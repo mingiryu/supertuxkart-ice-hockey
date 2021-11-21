ff4d0b192fa962ac1037a483f6989b25eb67bbe3
mingiryu <mingi@opensyllabus.org>
2021-11-20 23:52:38 -0600

    Drop unused code

--- image_agent/planner.py
+++ image_agent/planner.py
@@ -151,30 +151,3 @@ def load_model():
         )
     )
     return r
-
-
-if __name__ == '__main__':
-    from .controller import control
-    from .utils import PyTux
-    from argparse import ArgumentParser
-
-    def test_planner(args):
-        # Load model
-        planner = load_model().eval()
-        pytux = PyTux()
-        for t in args.track:
-            steps, how_far = pytux.rollout(
-                t,
-                control,
-                planner=planner,
-                max_frames=1000,
-                verbose=args.verbose,
-            )
-            print(steps, how_far)
-        pytux.close()
-
-    parser = ArgumentParser("Test the planner")
-    parser.add_argument('track', nargs='+')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    args = parser.parse_args()
-    test_planner(args)
